dc7b5bb818b033eeedc1ae1660c644c556d9b58a
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-07 10:40:37 -0600

    added email and pword digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_182846) do
+ActiveRecord::Schema.define(version: 2020_12_07_163930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_12_02_182846) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "notes", "users"
