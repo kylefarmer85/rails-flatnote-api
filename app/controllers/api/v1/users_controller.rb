6c87f3c533d49340b187fde246260b5ecf85886c
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-08 09:59:55 -0600

    pre master merge

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,18 +2,12 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    
     user = User.create(username: params[:username], password: params[:password])
-    
     if user.save
-      
       payload = { user_id: user.id }
-
       token = JWT.encode(payload, 'my_secret', 'HS256')
-
         render json: { user: {id: user.id, username: user.username}, notes: user.notes, token: token}
     else
-        # render json: user.errors.full_messages
         render json: {error: user.errors.full_messages}, status: 401
     end
   end
